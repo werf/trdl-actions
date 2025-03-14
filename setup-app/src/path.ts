0b542277122fdad1178c2f5fae0c2579161f24e6
Aleksei Igrychev <aleksei.igrychev@palark.com>
2025-03-14 16:25:46 +0000

    path logic
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- setup-app/src/path.ts
+++ setup-app/src/path.ts
@@ -2,7 +2,6 @@ import { BinPathArgs, TrdlCli } from '../../lib/trdl-cli'
 import { addPath, debug, endGroup, getInput, info, startGroup } from '@actions/core'
 import { optionalToObject } from '../../lib/optional'
 import { getUpdateArgs, preset } from './preset'
-import { which } from '@actions/io'
 import { format } from 'util'
 
 interface inputs extends BinPathArgs {}
@@ -37,20 +36,9 @@ export async function Do(trdlCli: TrdlCli, p: preset) {
   debug(format(`merged(preset, inputs) args=%o`, inputs))
 
   await trdlCli.mustExist()
-
-  const whichPath = await which(args.repo, false)
-  debug(format(`"which" application path=%s`, whichPath))
+  
   const appPath = await trdlCli.binPath(args)
   debug(format(`"trdl bin-path" application path=%s`, appPath))
-
-  if (whichPath !== '') {
-    if (whichPath !== appPath) {
-      throw new Error(`Found path=${whichPath} is not matched with "trdl bin-path"=${appPath}`)
-    }
-    info('Modifying of $PATH skipped. Application is already exist in $PATH variable.')
-    endGroup()
-    return
-  }
 
   // add app to $PATH
   info('Modifying $PATH variable.')
